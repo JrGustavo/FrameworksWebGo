6d29f12717c4babbca2d32d2ba78b42d18d305c4
JrGustavo <gadolfoalvarado@gmail.com>
2025-01-10 23:10:39 -0500

    Update Server function default

--- Gin/main.go
+++ Gin/main.go
@@ -5,6 +5,13 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		//c.String(200, "*!Hello, World!*")
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
 	r.Run(":8080")
 
 }
